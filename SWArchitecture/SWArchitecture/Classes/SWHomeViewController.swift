e6160d35138044ca64a4541238935a75f52f9a0e
cellgit <kokora2120@gmail.com>
2019-09-23 09:38:39 +0800

    *009 添加多种模态转场样式

--- SWArchitecture/SWArchitecture/Classes/SWHomeViewController.swift
+++ SWArchitecture/SWArchitecture/Classes/SWHomeViewController.swift
@@ -98,9 +98,43 @@ extension SWHomeViewController: UITableViewDataSource,UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        // push
+//        let vc = SWMvpViewController.init()
+//        vc.hidesBottomBarWhenPushed = true
+//       self.navigationController?.pushViewController(vc, animated: true)
+        
+        // 带nav的presenter
+//        let vc = SWMvpViewController()
+//        vc.modalTransitionStyle = .coverVertical
+//        let nc = UINavigationController.init(rootViewController: vc)
+//        let appDelegate = UIApplication.shared.delegate
+//        appDelegate?.window??.rootViewController?.definesPresentationContext = true
+//        nc.modalPresentationStyle = .overCurrentContext
+//        appDelegate?.window??.rootViewController?.present(nc, animated: true, completion: nil)
+        
+        
+        // 不带nav的presenter,且iOS13顶部会留有空条
+//        let vc = SWMvpViewController.init()
+//        vc.modalTransitionStyle = .coverVertical
+//        vc.modalPresentationStyle = .overCurrentContext
+//        self.present(vc, animated: true, completion: nil)
+        
+        // 全部展示
+//        let vc = SWMvpViewController.init()
+//        vc.modalPresentationStyle = .fullScreen
+//        self.present(vc, animated: true, completion: nil)
+        
         let vc = SWMvpViewController.init()
-        vc.hidesBottomBarWhenPushed = true
-        self.navigationController?.pushViewController(vc, animated: true)
+        vc.modalPresentationStyle = .automatic
+        self.present(vc, animated: true, completion: nil)
+        
+        
+        // 不带nav的presenter,iOS13顶部不会留有空条
+//        let vc = SWMvpViewController.init()
+//        let appDelegate = UIApplication.shared.delegate
+//        appDelegate?.window??.rootViewController?.definesPresentationContext = true
+//        vc.modalPresentationStyle = .overCurrentContext
+//        appDelegate?.window??.rootViewController?.present(vc, animated: true, completion: nil)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
